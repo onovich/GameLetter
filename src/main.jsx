1be05a161d2b3528c9461ee75c11e5d61f11cf04
onovich <hjn1110@163.com>
2026-04-23 00:24:14 +0800

    refactor: share cms shell styles with browse mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '../studio/app.css';
 import './styles.css';
 
 class ErrorBoundary extends React.Component {
